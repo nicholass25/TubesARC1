f4eac13df3456b30d5c6e3594ff93dcf1bb69226
Kent <71258048+kentlius@users.noreply.github.com>
2022-02-02 01:01:22 +0700

    add mongo_uri biar bisa jalan :)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: ""
+        MONGO_URI: "mongodb+srv://dbUser:cpUHb1uVq0iS4jZN@cluster0.fyx1p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     }
 }
