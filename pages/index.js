c07133ccf5b853939c9e392ed8be8e8de85bbba5
Kent <71258048+kentlius@users.noreply.github.com>
2022-02-01 23:56:21 +0700

    add api

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 import { Button, Card } from 'semantic-ui-react';
 
 const Index = ({ notes }) => {
@@ -6,30 +7,39 @@ const Index = ({ notes }) => {
     <div className="notes-container">
       <h1>Notes</h1>
       <div className="grid wrapper">
-        
-            <div>
+        {notes.map(note => {
+          return (
+            <div key={note._id}>
               <Card>
                 <Card.Content>
                   <Card.Header>
-                    <Link href="#">
-                      <a>test</a>
+                    <Link href={`/${note._id}`}>
+                      <a>{note.title}</a>
                     </Link>
                   </Card.Header>
                 </Card.Content>
                 <Card.Content extra>
-                  <Link href="#">
+                  <Link href={`/${note._id}`}>
                     <Button primary>View</Button>
                   </Link>
-                  <Link href="#">
+                  <Link href={`/${note._id}/edit`}>
                     <Button primary>Edit</Button>
                   </Link>
                 </Card.Content>
               </Card>
             </div>
-         
+          )
+        })}
       </div>
     </div>
   )
+}
+
+Index.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/notes');
+  const { data } = await res.json();
+
+  return { notes: data }
 }
 
 export default Index;
